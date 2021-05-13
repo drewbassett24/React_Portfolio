43022a8df4f5e9e86783f19aef2da8d7db2cacc3
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-13 17:55:26 +0100

    Got the banner displaying properly

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -2,8 +2,17 @@ import React from "react";
 import landscapetree2 from "./landscapetree2.jpg";
 
 const Banner = () => {
-    return <img src={landscapetree2} alt="landscape picture with a tree" />;
-
+    return (
+        <div>
+            <div>
+                <img src={landscapetree2} alt="landscape picture with a tree" />
+            </div>
+            <div class="card-img-overlay text-white">
+                <h1 id="card-title" class="card-title ">Drew Bassett</h1>
+                <h6 id="card-text" class="card-text">Hi. Welcome to my portfolio.</h6>
+            </div>
+        </div>
+    );
 }
 
 export default Banner;
