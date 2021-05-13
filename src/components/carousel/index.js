a86403bf5a21d77711194df640c2d33dde9d71a1
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-13 17:54:35 +0100

    Created a Carousel component

---
+++ src/components/carousel/index.js
@@ -0,0 +1,40 @@
+const Carousel = () => {
+    return (
+        <div className="Container">
+            <div className="d-flex">
+                <div id="carouselWithControls" className="carousel slide" data-ride="carousel">
+                    <div className="carousel-inner ">
+                        <div className="carousel-item active">
+                            <img src="./images/carousel1.jpg" className="d-block mx-auto w-25 h-25" alt="...">
+          </div>
+                            <div className="carousel-item">
+                                <img src="./images/carousel2(2).jpg" className="d-block mx-auto w-25 h-25" alt="...">
+          </div>
+                                <div className="carousel-item">
+                                    <img src="./images/carousel3.jpg" className="d-block mx-auto w-25 h-25" alt="...">
+          </div>
+                                    <div className="carousel-item">
+                                        <img src="./images/carousel4(2).jpg" className="d-block mx-auto w-25 h-25" alt="...">
+          </div>
+                                        <div className="carousel-item">
+                                            <img src="./images/carousel5.jpg" className="d-block mx-auto w-25 h-25" alt="...">
+          </div>
+                                            <!-- <div className="carousel-item">
+                                                <img src="./images/carousel6.jpg" className="d-block w-25 h-25" alt="...">
+          </div> -->
+        </div>
+                                            <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                                <span className="sr-only">Previous</span>
+                                            </a>
+                                            <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                                <span className="sr-only">Next</span>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+     );
+}
+
+export default Carousel;
