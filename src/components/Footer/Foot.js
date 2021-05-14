c2d1e521cdb2ac02478787b73deac6fa8429e457
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-14 08:47:43 +0100

    Created Footer

---
+++ src/components/Footer/Foot.js
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+
+        <div class="card">
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                    <p>"Don't listen to the nay-sayers."</p>
+                    <footer class="blockquote-footer">Arnold Schwarzenegger, <cite title="Source Title">Ted Talks.</cite>
+                    </footer>
+                </blockquote>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
