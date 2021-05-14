981f2610d0d43eb24467d5949e3b9a1ab05aff94
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-14 16:12:43 +0100

    tidied up Bio and got it working.

--- src/App.js
+++ src/App.js
@@ -4,30 +4,31 @@ import Navbar from "./components/Nav/index";
 import Banner from "./components/Banner/Banner.js";
 import Bio from "./components/Bio/bio.js";
 import Footer from "./components/Footer/Foot.js";
-import "./landscapetree2.jpg"; 
+import "./landscapetree2.jpg";
 
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Banner />
-      <section className="border rounded p-5">
-      <div className="row justify-content-center">
-        <h2> About Me </h2>
-      </div>
+      <div class="container-fluid card bg-light">
+        <Banner />
+          <div className="border rounded p-5">
+            <div className="row justify-content-center">
+              <h2> About Me </h2>
+            </div>
 
-      <div className="row border rounded ">
-        
+            <div className="row border rounded ">
 
-        <div className="col-8 m-4 p-3">
-          {/* <h3></h3> */}
-          <p>Hi. Thanks for taking the time to look at my page.</p>
-        </div>
-        <Bio/>
+              <div className="col-8 m-4 p-3">
+                {/* <h3></h3> */}
+                <p>Hi. Thanks for taking the time to look at my page.</p>
+              </div>
+              <Bio />
+            </div>
+          </div>
+          <Footer />
       </div>
-      </section>
-    <Footer/>
     </div>
   );
 }
--- src/components/Bio/bio.js
+++ src/components/Bio/bio.js
@@ -4,28 +4,28 @@ import me from "./me.jpg";
 const Bio = () => {
     return (
         <>
-            <div className="col-3 m-4 p-3">
-                <img src={me} alt="Drew"/>
-            </div>
+            <div class="row border rounded ">
+                <div className="col-3 m-4 p-3">
+                    <img src={me} alt="Drew" />
+                </div>
 
-            <div className="col-8 m-4 p-3">
-                {/* <h3></h3> */}
-                <p>Hi. Thanks for taking the time to look at my page.</p>
-                <p>I'm fifty years old and both relocating to Northern Ireland and retraining. I was a teacher for
-                twenty years, teaching TEFL in Thailand and Columbia, ESOL (English for Speakers of
-                Other Languages) in a Further Education College, and finally Literature and Language in Secondary School.
-                Most of those switches have seen me outside my comfort zone and
-                I like that. It stops you getting stuck in a rut.
-                In FE College, I had excellent training in target-setting, challenging yourself, problem-solving and basically
-                embracing the 'Discomfort Zone' despite all your instincts to do otherwise.
-                In Secondary School I learned a lot about young people and myself and Shakespeare.
-                I love teaching, but for personal reasons, it is time to do something new. I also have a cat, called PB.
-                Family members disagree over what it stands for, but for me, it means Personal Business.
-                Those who have time for Adventures will get the reference.
-                </p>
-                <p>I also like walking, talking, reading, writing, drawing, watching (good) movies, Science Fiction  (The Expanse,
-                Altered Carbon, The Quantum Thief), Roleplaying Games and good food. I like real ale and single malt
+                <div className="col-8 m-4 p-3">
+                    <p>Hi. Thanks for taking the time to look at my page.</p>
+                    <p>I'm fifty years old and both relocating to Northern Ireland and retraining. I was a teacher for
+                    twenty years, teaching TEFL in Thailand and Columbia, ESOL (English for Speakers of
+                    Other Languages) in a Further Education College, and finally Literature and Language in Secondary School.
+                    Most of those switches have seen me outside my comfort zone and
+                    I like that. It stops you getting stuck in a rut.
+                    In FE College, I had excellent training in target-setting, challenging yourself, problem-solving and basically
+                    embracing the 'Discomfort Zone' despite all your instincts to do otherwise.
+                    In Secondary School I learned a lot about young people and myself and Shakespeare.
+                    I love teaching, but for personal reasons, it is time to do something new. I also have a cat, called PB.
+                    Family members disagree over what it stands for, but for me, it means Personal Business.
+                    Those who have time for Adventures will get the reference.</p>
+                    <p>I also like walking, talking, reading, writing, drawing, watching (good) movies, Science Fiction  (The Expanse,
+                    Altered Carbon, The Quantum Thief), Roleplaying Games and good food. I like real ale and single malt
                 whiskey. And people. I like people.</p>
+                </div>
             </div>
         </>
     );
