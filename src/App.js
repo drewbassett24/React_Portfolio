f8c68c455740507bdaf6dbac79b0da24879015b7
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-14 17:05:25 +0100

    tidied some code

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Nav/index";
-import Banner from "./components/Banner/Banner.js";
-import Bio from "./components/Bio/bio.js";
+import Banner from "./components/Banner/Banner";
+import Bio from "./components/Bio/bio";
+import Carousel from "./components/Carousel/index";
 import Footer from "./components/Footer/Foot.js";
 import "./landscapetree2.jpg";
 
@@ -19,6 +20,9 @@ function App() {
             </div>
             <div>
               <Bio />
+            </div>
+            <div>
+              <Carousel />
             </div>
           </div>
           <Footer />
