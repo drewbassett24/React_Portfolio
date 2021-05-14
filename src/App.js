f116af3a913fe276d056b430cbb60c3529109ca0
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-14 16:13:07 +0100

    Got rid of some code in App that was duplicated in Bio

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Nav/index";
 import Banner from "./components/Banner/Banner.js";
 import Bio from "./components/Bio/bio.js";
@@ -13,23 +14,15 @@ function App() {
       <Navbar />
       <div class="container-fluid card bg-light">
         <Banner />
-          <div className="border rounded p-5">
             <div className="row justify-content-center">
               <h2> About Me </h2>
             </div>
-
-            <div className="row border rounded ">
-
-              <div className="col-8 m-4 p-3">
-                {/* <h3></h3> */}
-                <p>Hi. Thanks for taking the time to look at my page.</p>
-              </div>
+            <div>
               <Bio />
             </div>
           </div>
           <Footer />
       </div>
-    </div>
   );
 }
 
