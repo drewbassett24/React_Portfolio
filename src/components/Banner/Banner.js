9d727a81bc806881b30a42bc042fc370201c256a
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-14 11:17:11 +0100

    edit ClassName

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,17 +1,17 @@
-import React from "react";
+// import React from "react";
 import landscapetree2 from "./landscapetree2.jpg";
 
 const Banner = () => {
     return (
-        <div>
+        <>
             <div>
-                <img src={landscapetree2} alt="landscape picture with a tree" />
+                <img src={landscapetree2} alt="landscape with tree" />
             </div>
-            <div class="card-img-overlay text-white">
-                <h1 id="card-title" class="card-title ">Drew Bassett</h1>
-                <h6 id="card-text" class="card-text">Hi. Welcome to my portfolio.</h6>
+            <div className="card-img-overlay text-white">
+                <h1 id="card-title" className="card-title ">Drew Bassett</h1>
+                <h6 id="card-text" className="card-text">Hi. Welcome to my portfolio.</h6>
             </div>
-        </div>
+        </>
     );
 }
 
