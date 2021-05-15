53dee37fbfd19e1e3a2289417125720d87de3135
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-15 11:54:49 +0100

    created a Home compnent

---
+++ src/components/Home/home.js
@@ -0,0 +1,16 @@
+import Bio from "../Bio/bio";
+import Carousel from "../Carousel/carousel";
+const Home = () => {
+    return (
+        <div>
+            <div>
+                <Bio />
+            </div>
+            <div>
+                <Carousel />
+            </div>
+        </div>
+    );
+}
+
+export default Home;
