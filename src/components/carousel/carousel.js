dabc3d188aef0b497ad06ebbb605b224920c72c4
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-16 23:00:38 +0100

    Carousel working.

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,43 +1,40 @@
-import carousel1 from "./CarouselImgs/carousel1.jpg";
-import carousel2 from "./CarouselImgs/carousel2.jpg";
-import carousel3 from "./CarouselImgs/carousel3.jpg";
-import carousel4 from "./CarouselImgs/carousel4.jpg";
-import carousel5 from "./CarouselImgs/carousel5.jpg";
+import React from "react";
+import { UncontrolledCarousel } from "reactstrap";
+import carousel1 from "./images/carousel1.jpg";
+import carousel2 from "./images/carousel2.jpg";
+import carousel3 from "./images/carousel3.jpg";
+import carousel4 from "./images/carousel4.jpg";
+import carousel5 from "./images/carousel5.jpg";
 
-const Carousel = () => {
-    return (
-        <div className="Container">
-            <div className="d-flex">
-                <div id="carouselWithControls" className="carousel slide" data-ride="carousel">
-                    <div className="carousel-inner ">
-                        <div className="carousel-item active">
-                            <img src={carousel1} className="d-block mx-auto w-25 h-25" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={carousel2} className="d-block mx-auto w-25 h-25" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={carousel3} className="d-block mx-auto w-25 h-25" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={carousel4}  className="d-block mx-auto w-25 h-25" alt="..." />
-                        </div>
-                        <div className="carousel-item">
-                            <img src={carousel5} className="d-block mx-auto w-25 h-25" alt="..." />
-                        </div>
-                    </div>
-                    <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    );
-}
+const items = [
+    {
+        src: carousel1,
+        altText: "image 1",
+        key: '1'
+    },
+    {
+        src: carousel2,
+        altText: "image 2",
+        key: '2'
+    },
+    {
+        src: carousel3,
+        altText: "image 3",
+        key: '3'
+    },
+    {
+        src: carousel4,
+        altText: "image 4",
+        key: '4'
+    },
+    {
+        src: carousel5,
+        altText: "image 5",
+        key: '5'
+    },
 
+];
+
+const Carousel = () => <UncontrolledCarousel items={items} />;
+ 
 export default Carousel;
