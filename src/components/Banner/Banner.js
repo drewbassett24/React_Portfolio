6089787b269d77db738dab9779e823c9e82cca91
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-16 15:08:00 +0100

    got image displaying correctly

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,12 +4,14 @@ import landscapetree2 from "./landscapetree2.jpg";
 const Banner = () => {
     return (
         <>
-            <div>
-                <img src={landscapetree2} alt="landscape with tree" />
-            </div>
-            <div className="card-img-overlay text-white">
-                <h1 id="card-title" className="card-title ">Drew Bassett</h1>
-                <h6 id="card-text" className="card-text">Hi. Welcome to my portfolio.</h6>
+            <div class="container-fluid card bg-light">
+                <div className = "card image image-fluid">
+                    <img src={landscapetree2} alt="landscape with tree" />
+                </div>
+                <div className="card-img-overlay text-white">
+                    <h1 id="card-title" className="card-title ">Drew Bassett</h1>
+                    <h6 id="card-text" className="card-text">Hi. Welcome to my portfolio.</h6>
+                </div>
             </div>
         </>
     );
