5d97026b2bfc6860907eb243ff64dd3963a4cd7d
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-16 16:36:55 +0100

    renamed navbar file to navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Nav/index";
+import Navbar from "./components/Nav/navbar";
 import Banner from "./components/Banner/banner";
 import Home from "./components/Home/home";
 import Portfolio from "./components/Portfolio/portfolio";
+import Contact from "./components/Contact/contact";
 import Footer from "./components/Footer/footer.js";
 import "./landscapetree2.jpg";
 
@@ -16,16 +17,15 @@ function App() {
       <div className="App">
         <Navbar />
         <div class="container-fluid card bg-light">
-        <Banner />
+        <Home/>
           <Switch>
-            <Route path="./Home">
-              <Home/>
-            </Route>
             <Route path="./Portfolio">
               <Portfolio/>
             </Route>
+            <Route path="./Contact">
+              <Contact/>
+            </Route>
           </Switch>
-         
           <Footer />
         </div>
       </div>
