5c290e6a74269154a98f2ce7aca83b9e671b8388
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-05-28 18:35:16 +0100

    Trying to get Navbar to work

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar/navbar";
-import Banner from "./components/Banner/banner";
-import Home from "./components/Home/home";
-import Projects from "./components/Projects";
+import Navbar from "./components/Navbar/Navbar";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Project from "./components/Projects/Projects.js";
 // import Portfolio from "./components/Portfolio/portfolio";
-import Contact from "./components/Contact/contact";
-import Footer from "./components/Footer/footer.js";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer.js";
 import "./landscapetree2.jpg";
 
 
@@ -16,18 +16,15 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <Header />
         <div class="container-fluid card bg-light">
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/Portfolio">
-              <Projects />
+            <Route exact path="/Projects">
+              <Project />
             </Route>
-            {/* <Route exact path="/Projects">
-              <Projects />
-            </Route> */}
             <Route exact path="/Contact">
               <Contact />
             </Route>
--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,38 +1,27 @@
-import React from "react";
+  import React, { useRef } from "react";
 import { NavItem } from "reactstrap";
 
 const Navbar = () => {
-  import React, { useRef } from "react"
-  import Navitem from "../NavItem";
-
-  const Navbar = () => {
-    const toggleRef = useRef();
-    const menuRef = useRef();
+  const toggleRef = useRef();
+  const menuRef = useRef();
 
 
-    return (
-      <nav className="nav body">
-        <div>
-          <a href="#Home" className="nav-header">
-            Drew Bassett
+  return (
+    <nav className="nav body">
+      <div>
+        <a href="#Home" className="nav-header">
+          Drew Bassett
             </a>
-        </div>
+      </div>
 
-        <NavItem
-          links={["Home", "Projects", "Contact"]}
-          toggler={menuRef}
+      <NavItem
+        links={["Home", "Projects", "Contact"]}
+        
         />
 
-        <div id="nav-toggle" ref={toggleRef}>
-          <i
-            className="bx bx-menu"
-            onClick={() => {
-              menuRef.current.classlist.toggle("show");
-            }}
-          ></i>
-        </div>
-      </nav>
-    );
-  };
+      
+    </nav>
+  );
+};
 
-  export default Navbar;
+export default Navbar;
