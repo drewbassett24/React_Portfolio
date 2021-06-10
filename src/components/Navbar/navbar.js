e9cf96d9856f61f1737435194d47ca96e9d5674a
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-06-10 19:40:34 +0100

    Got navbar to appear correctly

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,27 +1,50 @@
-  import React, { useRef } from "react";
-import { NavItem } from "reactstrap";
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const toggleRef = useRef();
-  const menuRef = useRef();
-
-
-  return (
-    <nav className="nav body">
-      <div>
-        <a href="#Home" className="nav-header">
-          Drew Bassett
-            </a>
-      </div>
-
-      <NavItem
-        links={["Home", "Projects", "Contact"]}
-        
-        />
-
-      
-    </nav>
-  );
-};
+    return (
+        < nav className="navbar navbar-expand-lg navbar-light bg-light shadow mb-3" >
+            <div className="container-fluid">
+                <Link className="navbar-brand" to="/">Drew Bassett</Link>
+                
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li className="nav-item">
+                        <Link
+                            to="/"
+                            className={
+                                window.location.pathname === "/" || window.location.pathname === "/about"
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >About</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/portfolio"
+                                className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+                            >
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/contact"
+                                className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+}
 
 export default Navbar;
