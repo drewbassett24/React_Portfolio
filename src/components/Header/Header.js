10666213ce7a3b1be01cb319eaf7c3fb519bf04a
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-08-17 21:28:27 +0100

    deleted text overlay

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "../Navbar/Navbar.js"
 import landscapetree2 from "./landscapetree2.jpg";
 
+
 const Header = () => {
     return (
         <>
@@ -10,10 +11,10 @@ const Header = () => {
                 <div className = "card image image-fluid">
                     <img src={landscapetree2} alt="landscape with tree" />
                 </div>
-                <div className="card-img-overlay text-white">
+                {/* <div className="card-img-overlay text-white">
                     <h1 id="card-title" className="card-title ">Drew Bassett</h1>
                     <h6 id="card-text" className="card-text">Hi. Welcome to my portfolio.</h6>
-                </div>
+                </div> */}
             </div>
         </>
     );
