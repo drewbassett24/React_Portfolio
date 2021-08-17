5f0ac08dfbcfbe63a2869278d70bdef4f9bdc8a0
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-08-17 21:26:33 +0100

    removed margin bottom

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { NavItem } from "../NavItem/NavItem";
 
 const Navbar = () => {
     return (
-        < nav className="navbar navbar-expand-lg navbar-light bg-light shadow mb-3" >
+        < nav className="navbar navbar-expand-sm navbar-light bg-light shadow mb-3" >
             <div className="container-fluid">
                 <Link className="navbar-brand" to="/">Drew Bassett</Link>
                 
@@ -13,8 +14,8 @@ const Navbar = () => {
                 </button>
                 
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
+                    <ul className="navbar-nav me-auto mb-lg-0">
+                        <li className="nav-item"> 
                         <Link
                             to="/"
                             className={
