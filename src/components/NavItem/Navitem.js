2f8b081c9e2639d3268dbfa5918654b3b4213450
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-08-17 21:29:13 +0100

    deleted unnecessary export

--- src/components/NavItem/Navitem.js
+++ src/components/NavItem/Navitem.js
@@ -1,11 +1,11 @@
 import { useState } from "react";
 
-const NavItem = (props) => {
+export const NavItem = (props) => {
   const [appState, changeState] = useState({
     activeObject: null,
     objects: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }, { id: 5 }, { id: 6 } ],
   });
 
- 
+}
 
-export default NavItem;
+// export default NavItem;
