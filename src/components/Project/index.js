04fabe9f9f8a65a4dd152e4466a4efd1c8bfd885
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-08-26 20:46:11 +0100

    got text centred

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,7 +1,7 @@
 const Project = (props) => {
   return (
-    <div className="project-img d-flex">
-      <img src={props.img} className="screenshot justify-center" alt={props.alt} />
+    <div className="project-img d-flex col-12 mb-5">
+      <img src={props.img} className="screenshot mx-auto d-block" alt={props.alt} />
       <a href={props.deployed} target="_blank" rel="noreferrer noopener" className="display-flex">
           <div className="repo-btn display-flex">
             <a href={props.repo} target="_blank" rel="norefferer noopener" className="home-social-icon" >
@@ -9,8 +9,8 @@ const Project = (props) => {
             </a>
           </div>
       </a>
-          <div className="h2 justify-center:text-center">{props.title}</div>
-          <div className="justifyContent: text-center">{props.descrip}</div>
+      <div className="h2 justify-center">{props.title}</div>
+      <div className="justify-center">{props.descrip}</div>
 
     </div>
     
