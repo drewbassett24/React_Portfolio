4eb1945269c8dece2cb876550ca1ce274c5618e6
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-08-26 08:34:37 +0100

    deleted comments

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,10 +11,6 @@ const Header = () => {
                 <div className = "card image image-fluid">
                     <img src={landscapetree2} alt="landscape with tree" />
                 </div>
-                {/* <div className="card-img-overlay text-white">
-                    <h1 id="card-title" className="card-title ">Drew Bassett</h1>
-                    <h6 id="card-text" className="card-text">Hi. Welcome to my portfolio.</h6>
-                </div> */}
             </div>
         </>
     );
