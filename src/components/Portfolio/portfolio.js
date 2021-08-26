5309c3cce517ac1d9a1c6d527c4cad1877947b6c
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-08-26 08:35:56 +0100

    tidied up code

--- src/components/Portfolio/portfolio.js
+++ src/components/Portfolio/portfolio.js
@@ -4,7 +4,6 @@ import Projects from "../Projects/Projects.js"
 const Portfolio = () => {
     return (
         <div>
-            
             <main className="container-fluid card bg-light">
                 <Projects />
             </main>
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,16 +1,17 @@
 const Project = (props) => {
   return (
-    <div className="project-img">
-      <a href={props.deployed} target="_blank" rel="noreferrer noopener">
-          <img src={props.img} className="screenshot" alt={props.alt} />
-          <div className="h2">{props.title}</div>
-          <div>{props.descrip}</div>
-          <div className="repo-btn">
+    <div className="project-img d-flex">
+      <img src={props.img} className="screenshot justify-center" alt={props.alt} />
+      <a href={props.deployed} target="_blank" rel="noreferrer noopener" className="display-flex">
+          <div className="repo-btn display-flex">
             <a href={props.repo} target="_blank" rel="norefferer noopener" className="home-social-icon" >
-            <i className="bi bi-github"></i>
+            <i className="bi bi-github justify-center"></i>
             </a>
           </div>
       </a>
+          <div className="h2 justify-center:text-center">{props.title}</div>
+          <div className="justifyContent: text-center">{props.descrip}</div>
+
     </div>
     
   );
